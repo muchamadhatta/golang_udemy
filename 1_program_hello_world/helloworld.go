067c7adff260b75cb51346dc3ae3fcffec9524a5
muchamadhatta <muchamadhatta96@gmail.com>
2022-12-28 20:01:40 +0700

    membuat tipe data dengan string di golang

--- 1_program_hello_world/helloworld.go
+++ 1_program_hello_world/helloworld.go
@@ -5,3 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+//perintah menjalankan golang menggunakan go run
+//untuk mengkompile menggunakan go build
---
+++ string.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// mencetak tipe data dengan string
+func main() {
+	fmt.Println("ata")
+	fmt.Println("muchamad hatta")
+}

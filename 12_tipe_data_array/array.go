fa5f94864a1cebe22beca68045358d4057d33743
muchamadhatta <muchamadhatta96@gmail.com>
2022-12-30 03:54:10 +0700

    array

---
+++ 12_tipe_data_array/array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+
+	names[0] = "ata"
+	names[1] = "ataa"
+	names[2] = "ataaaa"
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+}

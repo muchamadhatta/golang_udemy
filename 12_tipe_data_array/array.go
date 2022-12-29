584b36f952a7b03bee2e14889faa520ad2bd10a9
muchamadhatta <muchamadhatta96@gmail.com>
2022-12-30 04:03:02 +0700

    array lagi

--- 12_tipe_data_array/array.go
+++ 12_tipe_data_array/array.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// index array di mulai dari nol
 func main() {
 	var names [3]string
 
@@ -11,4 +12,22 @@ func main() {
 	fmt.Println(names[0])
 	fmt.Println(names[1])
 	fmt.Println(names[2])
+
+	//menyimpan 3 array
+	var values = [3]int{
+		90,
+		95,
+		80,
+	}
+	fmt.Println(values[0])
+	fmt.Println(values[1])
+	fmt.Println(values[2])
+
+	//menghitung panjang array
+	fmt.Println(len(names))
+	fmt.Println(len(values))
+
+	//menghitung panjang string di array
+	var lagi [10]string //hasilnya akan 10 karena panjang array 10
+	fmt.Println(len(lagi))
 }

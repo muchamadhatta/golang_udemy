ce2eda3969f3158f6d9d8c2c306f0b39182c6e5f
muchamadhatta <muchamadhatta96@gmail.com>
2022-12-31 02:08:31 +0700

    delete map

--- 14_tipe_data_map/map.go
+++ 14_tipe_data_map/map.go
@@ -7,7 +7,22 @@ func main() {
 		"name":    "ata",
 		"address": "pamulang",
 	}
+
+	//nambah data
+	person["title"] = "Programmer"
+
 	fmt.Println(person)
 	fmt.Println(person["name"])
 	fmt.Println(person["address"])
+	// map dengan fungsi make
+	var book map[string]string = make(map[string]string)
+	book["title"] = "Belajar Go-Lang"
+	book["author"] = "Ata"
+	book["ups"] = "Salah"
+
+	fmt.Println(book)
+
+	//menghapus map
+	delete(book, "ups")
+	fmt.Println(book)
 }

7dba2e3c45d41445d53c53251760af95f1f0697d
muchamadhatta <muchamadhatta96@gmail.com>
2023-01-01 23:11:31 +0700

    else

---
+++ 15_percabangan_ if/else.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "ataa"
+	if name == "ata" {
+		fmt.Println("Hello ata")
+	} else {
+		fmt.Println("Hi, Boleh Kenalan?")
+	}
+
+}
---
+++ 15_percabangan_ if/if.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "ata" //true
+	// var name =="julio" //false
+	if name == "ata" {
+		fmt.Println("Hello Ata")
+	}
+}
